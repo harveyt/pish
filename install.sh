a29ae4906e42e8840b150564bf26f5e0780c9b1a
Harvey Thompson <harv.thompson@icloud.com>
2018-07-30 11:56:05 -0700

    Added 1pass_login requirement and use in install.

--- install.sh
+++ install.sh
@@ -7,20 +7,47 @@
 #
 
 PISH_URL=https://github.com/harveyt/pish.git
-PISH_ROOT=$HOME/Projects/pish-TEST
-PISH_LIB=$PISH_ROOT/lib/pish
-PISH=$PISH_ROOT/bin/pish
+PISH_LOCAL_BIN=/usr/local/bin
+PISH_LOCAL_LIB=/usr/local/lib/pish
+PISH_PROJECT_ROOT=$HOME/Projects/pish
+PISH_PROJECT_EXEC=$PISH_PROJECT_ROOT/lib/pish/exec
 
 # ================================================================================
 # Install
 #
 
+function ensure_1pass_binary()
+{
+    local name="$1" env="$2"
+    local binpath
+    
+    if [[ -x $PISH_LOCAL_BIN/$name ]]; then
+	echo "Using $name from $PISH_LOCAL_BIN ..."
+	binpath=$PISH_LOCAL_BIN/$name
+    else
+	echo "Using $name from $PISH_PROJECT_EXEC ..."
+	binpath=$PISH_PROJECT_EXEC/$name
+    fi
+    eval export $env=$binpath
+}
+
 function ensure_pish_installed()
 {
-    [[ -d $PISH_ROOT ]] && return
-    echo "Installing pish from $PISH_URL to $PISH_ROOT ..."
-    git clone $PISH_URL $PISH_ROOT
-    echo ""    
+    if [[ -d $PISH_LOCAL_LIB ]]; then
+	echo "Using PISH_ROOT=$PISH_LOCAL_LIB ..."
+	PISH_ROOT=$PISH_LOCAL_LIB
+    else
+	if [[ ! -d $PISH_PROJECT_ROOT ]]; then
+	    echo "Installing pish from $PISH_URL to $PISH_PROJECT_ROOT ..."
+	    git clone $PISH_URL $PISH_PROJECT_ROOT
+	fi
+	echo "Using PISH_ROOT=$PISH_PROJECT_ROOT ..."
+	PISH_ROOT=$PISH_PROJECT_ROOT
+    fi
+
+    ensure_1pass_binary 1pass _1PASS
+    ensure_1pass_binary op _1PASS_OP
+    ensure_1pass_binary jq _1PASS_JQ
 }
 
 ensure_pish_installed
@@ -28,7 +55,16 @@ ensure_pish_installed
 # ================================================================================
 # Library
 
+PISH_LIB=$PISH_ROOT/lib/pish
+PISH=$PISH_ROOT/bin/pish
+
 . $PISH
+. $PISH_LIB/basic
+. $PISH_LIB/1pass
+
+# ================================================================================
+# Login
+#
 
 # ================================================================================
 # Main
@@ -36,7 +72,7 @@ ensure_pish_installed
 
 function converge_defaults()
 {
-    true
+    requirement 1pass_login
 }
 
 converge "$@"
