f5e7d899e77f8fe60f4758367304853104e9b08f
Harvey Thompson <harv.thompson@icloud.com>
2023-01-24 11:54:07 -0800

    Configuration is now in pish itself.

--- install.sh
+++ install.sh
@@ -8,13 +8,14 @@
 
 PISH_URL="https://github.com/harveyt/pish.git"
 PISH_ROOT="$HOME/Projects/pish"
+GIT_ORIGIN=harveyt
 
 # --------------------------------------------------------------------------------
 clone_pish()
 {
     if [[ ! -d $PISH_ROOT ]]; then
 	echo "Cloning Pish into '$PISH_ROOT'..."
-	git clone --origin=$USER $PISH_URL $PISH_ROOT
+	git clone --origin=$GIT_ORIGIN $PISH_URL $PISH_ROOT
     else
 	echo "Updating Pish at '$PISH_ROOT'..."
 	(cd $PISH_ROOT; git pull)
