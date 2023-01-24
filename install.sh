582cf5b7f537209961acf9f034434051b60d64ec
Harvey Thompson <harv.thompson@icloud.com>
2023-01-24 11:28:46 -0800

    Start rationalization of config.

--- install.sh
+++ install.sh
@@ -6,58 +6,10 @@
 # Configuration
 #
 
-PISH_URL=https://github.com/harveyt/pish.git
-PISH_ROOT=$HOME/Projects/pish
-PISH_BIN=$PISH_ROOT/bin
-PISH_LIB=$PISH_ROOT/lib/pish
-PISH_HOST=$(hostname)
+PISH_URL="https://github.com/harveyt/pish.git"
+PISH_ROOT="$HOME/Projects/pish"
 
 # --------------------------------------------------------------------------------
-#
-
-detect_box_os()
-{
-    case $(uname -s) in
-	Darwin)
-	    OS_CONFIG=macos
-	    case $PISH_HOST in
-		Harveys-MBP)
-		    BOX_CONFIG=box-spartan-macos
-		    ;;
-		*)
-		    echo "Unknown $OS_CONFIG host: $PISH_HOST" >&2
-		    exit 1
-		    ;;
-	    esac
-	    ;;
-	Linux)
-	    # Could be Linux or WSL2
-	    case $(uname -r) in
-		*-WSL2)
-		    OS_CONFIG=linux-wsl2
-		    case $PISH_HOST in
-			panther)
-			    BOX_CONFIG=box-panther-win
-			    ;;
-			*)
-			    echo "Unknown $OS_CONFIG host: $PISH_HOST" >&2
-			    exit 1
-			    ;;
-		    esac
-		    ;;
-		*)
-		    echo "Linux native (non-WSL2) not yet supported" >&2
-		    exit 1
-		    ;;
-	    esac
-	    ;;
-	*)
-	    echo "Unknown operating system: $(uname -s)" >&2
-	    exit 1
-	    ;;
-    esac
-}
-
 clone_pish()
 {
     if [[ ! -d $PISH_ROOT ]]; then
@@ -71,12 +23,11 @@ clone_pish()
 
 run_pish()
 {
-    $PISH_BIN/pish --box="$BOX_CONFIG" --os="$OS_CONFIG"
+    $PISH_ROOT/bin/pish
 }
 
 # ================================================================================
 # Main
 
-detect_box_os
 clone_pish
 run_pish
